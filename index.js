4859ca0afdb5ede0ed37ba8b564294f0ae5de1d4
vladimka <zverevv.04@gmail.com>
2024-03-29 23:23:28 +0800

    telegraf added

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+
+const bot = new Telegraf(process.env.TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
